ad2a3f42e15054ffcc4955c33919cb380ef28351
Hanif Ramadhan <hramadhaan20@gmail.com>
2023-02-26 09:44:17 +0700

    #11 Setup Node JS

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const PORT = process.env.PORT || 8080
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+// Import Router configuration
+// Code Here
+
+const app = express()
+app.use(express.urlencoded({ extended: true }))
+app.use(express.json())
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader(
+        'Access-Control-Allow-Methods',
+        'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+    )
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization')
+    next()
+})
+
+// Initial Route Configuration
+// Code Here
+
+app.use((error, req, res, next) => {
+    const status = error.statusCode || 500
+    const message = error.data?.[0]?.msg || 'Server Failure'
+    res.status(status).json({
+        success: false,
+        message: message
+    })
+})
+
+mongoose
+    .connect(`mongodb+srv://${process.env.USER_NAME}:${process.env.USER_PASS}@cluster0.pnzdz4v.mongodb.net/?retryWrites=true&w=majority`)
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server running on port ${PORT}`)
+        })
+    })
